f8f51a69a07169182902be31d56d14822253bb98
Vovilonn <vovadesign0792@gmail.com>
2021-01-08 21:59:51 +0200

    class component was added

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,54 +1,65 @@
 import React from "react";
 import s from "./users.module.css";
 import * as axios from "axios";
-const Users = ({ users, follow, unfollow, setUsers }) => {
-    if (users.length === 0) {
+import userPhoto from "../../assets/images/user.png";
+
+class Users extends React.Component {
+    componentDidMount() {
         axios
             .get("https://social-network.samuraijs.com/api/1.0/users")
             .then((res) => {
-                console.log(res.data.items);
-                setUsers(res.data.items);
+                this.props.setUsers(res.data.items);
             });
     }
 
-    return (
-        <div>
-            {users.map((u) => (
-                <div key={u.id}>
-                    <span>
-                        <div>
-                            <img src="" alt="avatar" className={s.userPhoto} />
-                        </div>
-                        <div>
-                            {u.followed ? (
-                                <button
-                                    onClick={() => {
-                                        unfollow(u.id);
-                                    }}
-                                >
-                                    Unollow
-                                </button>
-                            ) : (
-                                <button
-                                    onClick={() => {
-                                        follow(u.id);
-                                    }}
-                                >
-                                    Follow
-                                </button>
-                            )}
-                        </div>
-                    </span>
-                    <span>
+    render() {
+        return (
+            <div>
+                {this.props.users.map((u) => (
+                    <div key={u.id}>
                         <span>
-                            <div>{u.name}</div>
-                            <div>{u.status}</div>
+                            <div>
+                                <img
+                                    src={
+                                        u.photos.small
+                                            ? u.photos.small
+                                            : userPhoto
+                                    }
+                                    alt="avatar"
+                                    className={s.userPhoto}
+                                />
+                            </div>
+                            <div>
+                                {u.followed ? (
+                                    <button
+                                        onClick={() => {
+                                            this.props.unfollow(u.id);
+                                        }}
+                                    >
+                                        Unollow
+                                    </button>
+                                ) : (
+                                    <button
+                                        onClick={() => {
+                                            this.props.follow(u.id);
+                                        }}
+                                    >
+                                        Follow
+                                    </button>
+                                )}
+                            </div>
                         </span>
-                    </span>
-                </div>
-            ))}
-        </div>
-    );
-};
+                        <span>
+                            <span>
+                                <div>{u.name}</div>
+                                <div>{u.status}</div>
+                            </span>
+                        </span>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
 
 export default Users;
